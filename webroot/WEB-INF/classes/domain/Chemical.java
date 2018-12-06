b9c58789d5e9115f447c49ca1314be6acd215811
Anna <annaakuzmina@gmail.com>
2018-12-06 21:41:34 +0100

    en kemikalieklass för parsning sen

---
+++ webroot/WEB-INF/classes/domain/Chemical.java
@@ -0,0 +1,10 @@
+
+public class Chemical {
+  private String name;
+  private String chemical_group;
+
+  public Chemical(String name, String chemical_group) {
+    this.name=name;
+    this.chemical_group=chemical_group;
+  }
+}
