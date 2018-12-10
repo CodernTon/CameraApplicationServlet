8a58675675f88f23e14da5f2c1c1d9ac84d62240
Anna <annaakuzmina@gmail.com>
2018-12-10 14:49:36 +0100

    Henrik skrev även ett skript för att kompilera och ladda

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CLASSPATH="webroot/WEB-INF/classes;webroot/WEB-INF/lib/org.json.jar;webroot/WEB-INF/lib/javax.servlet-api-3.1.0.jar"
+WINSTONE_JAR=winstone.jar
+JAVA_FILES="webroot/WEB-INF/classes/JsonFormatter.java webroot/WEB-INF/classes/Chemical.java webroot/WEB-INF/classes/ChemicalsServlet.java "
+
+
+compile () {
+  javac -cp ${CLASSPATH}  ${JAVA_FILES}
+  if [ $? -ne 0 ]
+  then
+    echo "COmpilation failed"
+    exit 1
+  fi
+}
+
+compile
+java -jar $WINSTONE_JAR --webroot=webroot
