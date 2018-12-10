50bff2ae4198d5e5720d5a0803e8ca5246f8d209
Anna <annaakuzmina@gmail.com>
2018-12-10 15:40:55 +0100

    Nu kan man söka flea kemikalier i samma URL

--- webroot/WEB-INF/classes/ChemicalsServlet.java
+++ webroot/WEB-INF/classes/ChemicalsServlet.java
@@ -13,7 +13,7 @@ import java.sql.*;
 
 import org.json.*;
 
-import domain.Chemical;
+//import domain.Chemical;
 
 public class ChemicalsServlet extends HttpServlet {
 
@@ -32,47 +32,50 @@ public class ChemicalsServlet extends HttpServlet {
     PrintWriter out = response.getWriter();
     out.println("<html><head><title>Chemicals servlet</title></head>");
     out.println("<body>");
-  String chemical =  request.getParameter("name");
 
-  Map<String, String[]> map =
-     request.getParameterMap();
+    Map<String, String[]> map =
+    request.getParameterMap();
+    List<Chemical> chemicals = new ArrayList<>();
+    List<String> names = new ArrayList<>();
 
-  for (Map.Entry<String, String[]> entry : map.entrySet()) {
-    out.println(" * key / value: " + entry.getKey() + " / " + entry.getValue()[0] + "<br />");
-    if (entry.getKey().equals("name")) {
-      for (int i=0; i<entry.getValue().length; i++) {
-        String value = entry.getValue()[i];
-        out.println(" ----" + value);
+    for (Map.Entry<String, String[]> entry : map.entrySet()) {
+      out.println(" * key / value: " + entry.getKey() + " / " + entry.getValue()[0] + "<br />");
+      if (entry.getKey().equals("name")) {
+        for (int i=0; i<entry.getValue().length; i++) {
+          String value = entry.getValue()[i];
+          out.println(" ----" + value);
+          names.add(value);
+        }
       }
     }
-  }
+    System.out.println("   names:" + names);
 
     try{
       Connection con = DriverManager.getConnection("jdbc:sqlite:kemi.db");
       Statement stm  = con.createStatement();
-      StringBuilder sb = new StringBuilder();
-      List<Chemical> chemList = new ArrayList<>();
-      sb.append("SELECT * from chemical WHERE name='");
-      sb.append(chemical);
-      sb.append("';");
-      String sql = sb.toString();
-      System.out.println(sql);
-      ResultSet rs   = stm.executeQuery(sql);
-      while(rs.next()){
-        Chemical chem = new Chemical(rs.getString("name"), rs.getString("chemical_group"));
-        //out.println(chem);
-        //JSONArray JSONArray = new JSONArray();
-        JSONObject JSONChemical = new JSONObject();
-        JSONChemical.put("name", chem.name());
-        JSONChemical.put("chemical_group", chem.chemGroup());
-        //JSONArray.put(JSONChemical);
-        String json = JSONChemical.toString(2);
-        out.println(json);
-
+      for (String chemical : names) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT * from chemical WHERE name='");
+        sb.append(chemical);
+        sb.append("';");
+        String sql = sb.toString();
+        System.out.println(sql);
+        ResultSet rs   = stm.executeQuery(sql);
+        System.out.println("SQL: " + sql );
+        while(rs.next()){
+          Chemical chem = new Chemical(rs.getString("name"), rs.getString("chemical_group"));
+          chemicals.add(chem);
+          System.out.println(" * " + rs.getString("name"));
+        }
+        System.out.println();
       }
     }catch(SQLException sqle)
     {out.println("Database error: " + sqle.getMessage());
   }
+  JsonFormatter formatter = new JsonFormatter();
+  String JSONString = formatter.format(chemicals);
+  out.println(JSONString);
+
   out.println("</body></html>");
   out.close();
 }
--- webroot/WEB-INF/classes/JsonFormatter.java
+++ webroot/WEB-INF/classes/JsonFormatter.java
@@ -7,40 +7,9 @@ import java.util.*;
 import java.io.*;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-import java.sql.*;
-
 import org.json.*;
 
 public class JsonFormatter {
-  public static void main(String [] args) {
-
-    try{
-      Class.forName("org.sqlite.JDBC");
-    }catch(ClassNotFoundException cnfe){
-      System.err.println("Could not load driver: "+cnfe.getMessage());
-    }
-
-    String chemicalArg = args[0];
-    List<Chemical> chemicals = new ArrayList<>();
-
-    try{
-      Connection con = DriverManager.getConnection("jdbc:sqlite:kemi.db");
-      Statement stm  = con.createStatement();
-      StringBuilder sb = new StringBuilder();
-      sb.append("SELECT * from chemical WHERE name='");
-      sb.append(chemicalArg);
-      sb.append("';");
-      String sql = sb.toString();
-      System.out.println(sql);
-      ResultSet rs   = stm.executeQuery(sql);
-      while(rs.next()){
-        Chemical chem = new Chemical(rs.getString("name"), rs.getString("chemical_group"));
-        chemicals.add(chem);
-      }
-    }catch(SQLException sqle)  {
-      System.out.println("Database error: " + sqle.getMessage());
-    }
-  }
 
   public JSONObject JSONChemical(Chemical chemical) {
     JSONObject JSONChemical = new JSONObject();
