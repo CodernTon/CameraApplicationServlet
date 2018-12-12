a3fa70266f1b33c8c8f2f3aad2afd2a821ff1b77
Anton Lyngfelt <antonlyngfelt@Antons-MacBook-Air.local>
2018-12-12 13:18:38 +0100

    testing to upload

---
+++ application_v1/app/src/main/java/se/ingoproduction/example/application_v1/ScanThePic.java
@@ -0,0 +1,23 @@
+package se.ingoproduction.example.application_v1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class ScanThePic extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_thepic);
+    }
+
+    public void goBackToMain(View view) {
+        Intent goBackToMainIntent = new Intent
+                (this, MainActivity.class);
+        startActivity(goBackToMainIntent);
+
+    }
+}
