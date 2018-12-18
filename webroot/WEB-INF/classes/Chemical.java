779418f95af0bc9b8d62af079b419c02b180e26c
Anna <annaakuzmina@gmail.com>
2018-12-18 12:47:09 +0100

    Builder löst

--- webroot/WEB-INF/classes/Chemical.java
+++ webroot/WEB-INF/classes/Chemical.java
@@ -2,23 +2,75 @@
 
 public class Chemical {
   private String name;
-  private String chemical_group;
+  private String casnr;
+  private String egnr;
+  private String priolevel;
+  private String criteria;
 
-  public Chemical(String name, String chemical_group) {
-    this.name=name;
-    this.chemical_group=chemical_group;
+
+  public static class ChemicalBuilder {
+    private String name;
+    private String casnr;
+    private String egnr;
+    private String priolevel;
+    private String criteria;
+
+    public ChemicalBuilder(String name, String criteria){
+    this.name = name;
+    this.criteria = criteria;
+  }
+
+    public ChemicalBuilder casNr(String casnr) {
+      this.casnr = casnr;
+      return this;
+    }
+
+    public ChemicalBuilder egNr(String egnr) {
+      this.egnr = egnr;
+      return this;
+    }
+
+    public ChemicalBuilder prioLevel(String priolevel) {
+      this.priolevel = priolevel;
+      return this;
+    }
+
+    public Chemical build() {
+      return new Chemical(this);
+    }
+  }
+
+  private Chemical(ChemicalBuilder cb) {
+    this.name = cb.name;
+    this.casnr = cb.casnr;
+    this.egnr = cb.egnr;
+    this.priolevel = cb.priolevel;
+    this.criteria = cb.criteria;
   }
 
   public String name() {
     return name;
   }
-
-  public String chemGroup() {
-    return chemical_group;
+  public String casnr() {
+    return casnr;
+  }
+  public String egnr() {
+    return egnr;
+  }
+  public String priolevel() {
+    return priolevel;
+  }
+  public String criteria() {
+    return criteria;
   }
 
   @Override
   public String toString() {
-    return "Name: " + this.name + "\n" + "Chemical group: " + this.chemical_group + "\n";
+    return "Name: " + this.name + "\n" +
+    "Criteria: " + this.criteria + "\n" +
+    "CAS number: " + this.casnr + "\n" +
+    "EG number: " + this.egnr + "\n" +
+    "Priority level: " + this.priolevel + "\n" +
+    "----------" + "\n";
   }
 }
