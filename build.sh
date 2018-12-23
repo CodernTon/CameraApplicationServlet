8eb49cdde61e0a3723814250efd0ce8889cfab7f
Anna <annaakuzmina@gmail.com>
2018-12-23 19:28:50 +0100

    lagt till nya message-klassen för kompilering

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 CLASSPATH="webroot/WEB-INF/classes;webroot/WEB-INF/lib/org.json.jar;webroot/WEB-INF/lib/javax.servlet-api-3.1.0.jar"
 WINSTONE_JAR=winstone.jar
-JAVA_FILES="webroot/WEB-INF/classes/JsonFormatter.java webroot/WEB-INF/classes/Chemical.java webroot/WEB-INF/classes/ChemicalsServlet.java "
+JAVA_FILES="webroot/WEB-INF/classes/JsonFormatter.java webroot/WEB-INF/classes/Chemical.java webroot/WEB-INF/classes/ChemicalsServlet.java webroot/WEB-INF/classes/Message.java"
 
 
 compile () {
   javac -cp ${CLASSPATH}  ${JAVA_FILES}
   if [ $? -ne 0 ]
   then
-    echo "COmpilation failed"
+    echo "Compilation failed"
     exit 1
   fi
 }
