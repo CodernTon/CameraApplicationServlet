b10dc0d4a46a035aba1ffe2298835cd6a49ff8dc
Anna <annaakuzmina@gmail.com>
2019-01-03 13:06:29 +0100

    ändrat skriptet

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 CLASSPATH="webroot/WEB-INF/classes;webroot/WEB-INF/lib/org.json.jar;webroot/WEB-INF/lib/javax.servlet-api-3.1.0.jar"
 WINSTONE_JAR=winstone.jar
-JAVA_FILES="webroot/WEB-INF/classes/JsonFormatter.java webroot/WEB-INF/classes/Chemical.java webroot/WEB-INF/classes/ChemicalsServlet.java webroot/WEB-INF/classes/Message.java"
+JAVA_FILES="webroot/WEB-INF/classes/JsonFormatter.java webroot/WEB-INF/classes/Chemical.java webroot/WEB-INF/classes/ChemicalsServlet.java"
 
 
 compile () {
